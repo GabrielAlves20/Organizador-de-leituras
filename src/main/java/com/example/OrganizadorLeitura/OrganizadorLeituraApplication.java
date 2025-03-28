fe5e3ffdb2273725bec34838e24a89dfb27e22f4
Gabriel Alves <gabriel12425@gmail.com>
2025-03-28 20:41:50 -0300

    Criação da main corrigida

---
+++ src/main/java/com/example/OrganizadorLeitura/OrganizadorLeituraApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrganizadorLeitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorLeituraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorLeituraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OrganizadorLeitura/OrganizadorLeituraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OrganizadorLeitura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorLeituraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
